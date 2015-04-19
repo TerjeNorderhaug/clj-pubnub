b55089dbe378a7978f6f8cbbd351041a78c6b0f6
Terje Norderhaug <terje@in-progress.com>
2015-04-18 20:25:29 -0700

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject clj-pubnub "0.0.1"
   :description "Clojure client for PubNub"
-  :dependencies [[clj-http "0.3.2"]
-                 [cheshire "2.2.2"]
-                 [digest "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-http "0.3.2"]
+                 [cheshire "5.4.0"]
+                 [digest "1.4.4"]]
   :dev-dependencies [[swank-clojure "1.4.0"]])
